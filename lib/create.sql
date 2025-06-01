22d2d6c8e1f555ac79ee789a4068e2e5947e0a53
*Collins <nyamburycollins@mail.com>
2025-06-01 07:47:35 +0300

    Completed

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+ id integer,
+ name TEXT,
+ age integer,
+ sex TEXT,
+ color TEXT,
+ temperament TEXT,
+ alive boolean
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES
+('Mr.Chocolate', 10, 'Male', 'white', 'friendly', '1'),
+('Rowdy', 18, 'Male', 'black', 'Energetic', '2'),
+('Tabitha', 12, 'Female', 'white', 'friendly', '1'),
+('Sergeant Brown', 14, 'Male', 'Brown', 'Dangerous', '0' ),
+('Melissa', 8, 'Female', 'Gray', 'vigorous', '1'),
+('Grinch', 5, 'Male', 'Black', 'friendly', '0'),
+('Wendy', 16, 'Female', 'White', 'Calm', '1'),
+(NULL, NULL, NULL, NULL, NULL, NULL);
